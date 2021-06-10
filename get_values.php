7ffb32e993aaf43f3c581112d9688e0355039cc9
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-06-10 10:44:57 +0530

    Update get_values.php

--- get_values.php
+++ get_values.php
@@ -1,5 +1,10 @@
 <?php
 
+$sid = array();
+$sname = array(); 
+$marks = array(); 
+$pass = array();
+
 require 'db_connect.php';
 
 // #boolean value = mysqli_connect(ip address, username, password, database)
@@ -15,15 +20,28 @@ require 'db_connect.php';
     echo "Connected to the database <br>";
     $query = "select * from students";
     $result = $conn->query($query);
+    //$result = mysqli_query($conn, $query);
 
     if ($result -> num_rows > 0)
     {
-        echo "Found some rows";
+        while ($arr = mysqli_fetch_array($result))
+        {
+            array_push($sid, $arr['sid']);
+            array_push($sname, $arr['sname']);
+            array_push($marks, $arr['marks']);
+            array_push($pass, $arr['pass']);
+        }
+        // $sname = $arr['sname'];
+        // $marks =  $arr['marks'];
+        // $pass = $arr['pass'];
     }
     else
     {
         echo "Found nothing";
     }
+
+    echo json_encode(array('sid' => $sid, 'sname' => $sname, 'marks' => $marks, 'pass' => $pass));
+
 // }
 
 ?>
